9c54b5156bf12a930d77ecebcaae2d5ff7cbcdc8
Noé Landré <68849024+DevNono@users.noreply.github.com>
2024-09-05 13:56:12 +0200

    Update api.php

--- api.php
+++ api.php
@@ -17,7 +17,7 @@ try {
     $allowed_extensions = ['png', 'jpg', 'pdf', 'webp'];
 
     if(empty($_GET) || empty($_GET['authorization'])) {
-        response(1, 'Paramètres manquants');
+        response(1, '[File Upload] Authentification manquante');
     }
 
     // Get the bearer token from the header with the Bearer prefix
@@ -25,26 +25,26 @@ try {
 
     // Check if the API_KEY is correct
     if ($bearer !== $key) {
-        response(1, 'Authentification échouée');
+        response(1, '[File Upload] Authentification échouée');
     }
 
     // Code for DELETE method
     if ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
         if(empty($_GET['path'])) {
-            response(1, 'Paramètres manquants');
+            response(1, '[File Upload] Paramètres manquants');
         }
 
         $path = '../' . strval(htmlspecialchars(filter_var($_GET['path'])));
 
         if(!in_array(explode('/', $path)[1], $allowed_paths)) {
-            response(1, 'Le chemin n\'est pas autorisé');
+            response(1, '[File Upload] Le chemin n\'est pas autorisé');
         }
 
         if (file_exists($path)) {
             unlink($path);
-            response(0, 'Fichier supprimé avec succès');
+            response(0, '[File Upload] Fichier supprimé avec succès');
         } else {
-            response(1, 'Le fichier n\'existe pas');
+            response(1, '[File Upload] Le fichier n\'existe pas');
         }
     }
 
@@ -52,12 +52,12 @@ try {
     // Test if $_POST contains everything we need
     if (empty($_FILES) || empty($_FILES['file']) || empty($_POST) || empty($_POST['name']) || empty($_POST['path'])) {
         // Return error
-        response(1, "Paramètres manquants");
+        response(1, "[File Upload] Paramètres manquants");
     }
 
     // Max file size is 5MB
     if ($_FILES['file']['size'] > 5000000) {
-        response(1, 'La taille maximale d\'un fichier est de 5MB');
+        response(1, '[File Upload] La taille maximale d\'un fichier est de 5MB');
     }
 
     // Check $_FILES['file']['error'] value.
@@ -65,19 +65,19 @@ try {
         case UPLOAD_ERR_OK:
             break;
         case UPLOAD_ERR_NO_FILE:
-            response(1, 'Aucun fichier n\'a été envoyé');
+            response(1, '[File Upload] Aucun fichier n\'a été envoyé');
         case UPLOAD_ERR_INI_SIZE:
         case UPLOAD_ERR_FORM_SIZE:
-            response(1, 'Limite de taille de fichier dépassée');
+            response(1, '[File Upload] Limite de taille de fichier dépassée');
         default:
-            response(1, 'Erreur inconnue');
+            response(1, '[File Upload] Erreur inconnue');
     }
 
     // Store the file in a variable
     $file = $_FILES['file'];
 
     if(!in_array($file['type'], $allowed_file_types)) {
-        response(1, 'Type de fichier non autorisé');
+        response(1, '[File Upload] Type de fichier non autorisé');
     }
 
     // Filter the parameters and store them in variables
@@ -87,7 +87,7 @@ try {
     $extension = pathinfo($file['name'], PATHINFO_EXTENSION);
 
     if(!in_array($extension, $allowed_extensions)) {
-        response(1, 'Extension de fichier non autorisée');
+        response(1, '[File Upload] Extension de fichier non autorisée');
     }
 
     // Get the full name of the file with the extension
@@ -98,12 +98,12 @@ try {
 
     // Check if the path exists
     if (!file_exists($path)) {
-        response(1, 'Le chemin n\'existe pas');
+        response(1, '[File Upload] Le chemin n\'existe pas');
     }
 
     // Check if the path is in the allowed paths
     if(!in_array(explode('/', $path)[1], $allowed_paths)) {
-        response(1, 'Le chemin n\'est pas autorisé');
+        response(1, '[File Upload] Le chemin n\'est pas autorisé');
     }
 
     // Check if the file already exists if it does, delete it
@@ -114,13 +114,13 @@ try {
     // Move the file to the path
     if (move_uploaded_file($file['tmp_name'], $file_path)) {
         // Return success
-        response(0, 'Fichier téléversé avec succès');
+        response(0, '[File Upload] Fichier téléversé avec succès');
     } else {
         // Return error
-        response(1, 'Erreur lors du téléversement du fichier');
+        response(1, '[File Upload] Erreur lors du téléversement du fichier');
     }
 }
 catch (\Error $e) {
    // do your catching
-    response(1, 'Erreur inconnue');
+    response(1, '[File Upload] Erreur inconnue');
 }
